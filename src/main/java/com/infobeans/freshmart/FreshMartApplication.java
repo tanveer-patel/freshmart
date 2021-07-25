ea319a9b12e7be68f825ae7501b33171df5ff96f
tanveer-patel <85062210+tanveer-patel@users.noreply.github.com>
2021-07-25 11:58:51 +0530

    make it better

---
+++ src/main/java/com/infobeans/freshmart/FreshMartApplication.java
@@ -0,0 +1,13 @@
+package com.infobeans.freshmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshMartApplication.class, args);
+	}
+
+}
